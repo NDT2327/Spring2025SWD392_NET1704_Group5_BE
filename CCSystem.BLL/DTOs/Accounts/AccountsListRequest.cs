970240ac88392d21537ad30a7cd95710c36933f9
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2025-02-17 11:06:30 +0700

    Adding view profile, get account list and update profile feature

---
+++ CCSystem.BLL/DTOs/Accounts/AccountsListRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSystem.BLL.DTOs.Accounts
+{
+    public class AccountsListRequest
+    {
+        //sort: email, email_desc, fullname, fullname_desc, createddate, createddate_desc, editeddate, editeddate_desc
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string SearchByName { get; set; }
+        public string Sort { get; set; }
+    }
+}
